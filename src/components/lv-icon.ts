89fe107077de682735b7acf506388892a7cb29ef
Ellie <ellie@fixzy.ai>
2026-04-29 12:47:50 +0100

    key icon

--- src/components/lv-icon.ts
+++ src/components/lv-icon.ts
@@ -97,6 +97,9 @@ const ICONS: Record<string, TemplateResult> = {
   sun: svg`
     <circle cx="12" cy="12" r="4" /><path d="M12 2v2M12 20v2M4.93 4.93l1.41 1.41M17.66 17.66l1.41 1.41M2 12h2M20 12h2M4.93 19.07l1.41-1.41M17.66 6.34l1.41-1.41" />
   `,
+  key: svg`
+    <circle cx="7.5" cy="14.5" r="4.5" /><path d="M11 11 21 1" /><path d="m16 6 2 2" /><path d="m19 3 2 2" />
+  `,
 };
 
 export class LvIcon extends LitElement {
